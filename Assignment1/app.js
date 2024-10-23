f8b35a2f2fc64377ba1290181835ee3056451972
Xinyu Ren <xinyu.ren@stud.uni-bamberg.de>
2024-10-23 23:45:49 +0200

    Added function Error Message in tab "Add Book". Fixed small bugs.

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -3,7 +3,7 @@
                 event.preventDefault();
                 const tabId = tab.getAttribute('href').substring(1);
                 const tabContent = document.getElementById(tabId);
-                document.querySelectorAll('.tab').forEach(item => {
+                document.querySelectorAll('.nav-link').forEach(item => {
                     item.classList.remove('active');
                 });
                 document.querySelectorAll('.content').forEach(content => {
@@ -13,3 +13,43 @@
                 tabContent.classList.add('active');
             });
         });
+
+        function showErrorMessage(message) {
+            var errorMessage = document.getElementById('error-message');
+            errorMessage.innerText = message;
+            errorMessage.style.display = 'block';
+            errorMessage.classList.add('show');
+
+            setTimeout(function() {
+                errorMessage.classList.remove('show');
+                errorMessage.style.display = 'none';
+            }, 3000);
+        }
+        function validateForm(){
+            var author = document.getElementById('author').value;
+            var title = document.getElementById('title').value;
+            var isbn = document.getElementById('isbn').value;
+            var description = document.getElementById('description').value;
+
+            if(author === ''){
+                showErrorMessage('Please fill in all fields');
+                return false;
+            }
+
+            if(title === ''){
+                showErrorMessage('Please fill in all fields');
+                return false;
+            }
+
+            if(isbn === ''){
+                showErrorMessage('Please fill in all fields');
+                return false;
+            }
+
+            if(description=== ''){
+                showErrorMessage('Please fill in all fields');
+                return false;
+            }
+
+            return true;
+        }
