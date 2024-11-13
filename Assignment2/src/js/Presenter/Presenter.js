d9f0526032029c500e0724e114be374f36b833da
Elena Laubinger <elena_laubinger@stud.uni-bamberg.de>
2024-11-13 15:47:46 +0100

    Finish function for choosing either validate or minify

--- Assignment2/src/js/Presenter/Presenter.js
+++ Assignment2/src/js/Presenter/Presenter.js
@@ -4,8 +4,36 @@ import { ChooseAction } from "../View/View.js"
 // Based on user's choice, different tasks and their progresses would be shown.
 export async function runTasks() {
   const action = await ChooseAction();
-  //If user chooses only to validate dependencies, following tasks would show.
+  //If user chooses to validate and minify, following tasks would show.
   if (action === 'validate') {
+    const tasks = new Listr([
+      {
+        title: 'Determine Links from the HTML File',
+        task: async () => {
+          await new Promise(resolve => setTimeout(resolve, 1000));
+        },
+      },
+      {
+        title: 'Check Dependencies',
+        task: async () => {
+          await new Promise(resolve => setTimeout(resolve, 1000));
+
+          const hasError = true;
+          const userHomeDir = os.homedir();
+          const directory = path.join(userHomeDir, 'myproject', 'doesNotExist.js');
+
+          if (hasError) {
+            throw new Error('Error happened - ENONENT: no such file or directory, access ' + directory);
+          }
+        },
+      },
+    ])
+    await tasks.run().catch(err => {
+      console.error('An error occurred:', err);
+    });
+  }
+  //If user chooses only to validate dependencies, following tasks would show.
+  if (action === 'minify') {
     const tasks = new Listr([
       {
         title: 'Determine Links from the HTML File',
@@ -67,35 +95,6 @@ export async function runTasks() {
       renderer: 'default',
     });
 
-    await tasks.run().catch(err => {
-      console.error('An error occurred:', err);
-    });
-  }
-
-  //If user chooses to validate and minify, following tasks would show.
-  if (action === 'minify') {
-    const tasks = new Listr([
-      {
-        title: 'Determine Links from the HTML File',
-        task: async () => {
-          await new Promise(resolve => setTimeout(resolve, 1000));
-        },
-      },
-      {
-        title: 'Check Dependencies',
-        task: async () => {
-          await new Promise(resolve => setTimeout(resolve, 1000));
-
-          const hasError = true;
-          const userHomeDir = os.homedir();
-          const directory = path.join(userHomeDir, 'myproject', 'doesNotExist.js');
-
-          if (hasError) {
-            throw new Error('Error happened - ENONENT: no such file or directory, access ' + directory);
-          }
-        },
-      },
-    ])
     await tasks.run().catch(err => {
       console.error('An error occurred:', err);
     });
