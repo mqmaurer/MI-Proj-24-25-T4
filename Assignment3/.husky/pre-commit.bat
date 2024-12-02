955de04bed1304eb81f705beb4a4037fd1858a6d
Monique Maurer <monique.maurer@stud.uni-bamberg.de>
2024-12-02 23:01:45 +0100

    shell

--- Assignment3/.husky/pre-commit.bat
+++ Assignment3/.husky/pre-commit.bat
@@ -1,2 +1,8 @@
-. "$(dirname "$0").husky/_/husky.sh"
-npm test
+#!/bin/sh
+# Check if on Windows and call .bat file
+if [ "$(uname -s)" = "MINGW32_NT" ] || [ "$(uname -s)" = "MINGW64_NT" ]; then
+  .husky/pre-commit.bat
+else
+  . "$(dirname "$0")/_/husky.sh"
+  npm run lint
+fi
