70a7a9304e60915bbd4c2b459ec12fdbee7ebe4c
Monique Maurer <monique.maurer@stud.uni-bamberg.de>
2024-12-05 01:41:17 +0100

    updated config

--- Assignment3/webpack.config.mjs
+++ Assignment3/webpack.config.mjs
@@ -62,7 +62,7 @@ export default (env, argv) => {
         fix: true, 
         files: ["src/**/*.js"], // Lintet alle JavaScript-Dateien im src-Ordner
         failOnError: isProduction, // Lässt den Build im Fehlerfall scheitern, wenn es sich um Produktion handelt
-       overrideConfigFile: 'eslint.config.mjs',}),
+       overrideConfigFile: './eslint.config.mjs',}),
     ],
 
     // Entwicklungs-Tools
