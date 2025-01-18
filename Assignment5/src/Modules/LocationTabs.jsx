6f52c1f267245d778cfdaf03481656d4c7d3726e
Elena Laubinger <elena_laubinger@stud.uni-bamberg.de>
2025-01-18 16:22:47 +0100

    Add basic program pattern

---
+++ Assignment5/src/Modules/LocationTabs.jsx
@@ -0,0 +1,18 @@
+const LocationTabs = () => {
+
+    return (
+        {/* <ul id="navigation" class="nav nav-tabs nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="#/books">Book List</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#/addBooks">Add Book</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#/details">Book Detail</a>
+            </li>
+        </ul> */}
+    );
+}
+
+export default LocationTabs;
