98a75e52cd418f1f0e379f1623ad4b259e4845d9
Xinyu Ren <xinyu.ren@stud.uni-bamberg.de>
2025-01-23 17:26:45 +0100

    Updated 404 page

--- Assignment5/src/Modules/NotFound.jsx
+++ Assignment5/src/Modules/NotFound.jsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom'
 import "../assets/css/NotFound.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 const NotFound = () => {
   return (
-    <div className="container mt-4">
-      <h2>404 - Not Found</h2>
-      <Link to="/">Back to book list</Link>
+    <div className="not-found-container">
+          <h1 className="not-found-title">404 - Not Found</h1>
+          <Link to="/" className="not-found-link">
+            <FontAwesomeIcon icon={faLink} /> Back to book list
+          </Link>
     </div>
   );
 };
+
 
 export default NotFound;
