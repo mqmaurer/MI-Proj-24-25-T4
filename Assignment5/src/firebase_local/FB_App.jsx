20c7581ea918351a5fd948a2eb1c8abf2bb991d7
Elena Laubinger <elena_laubinger@stud.uni-bamberg.de>
2025-01-23 17:06:12 +0100

    Minor import/rename changes

---
+++ Assignment5/src/firebase_local/FB_App.jsx
@@ -0,0 +1,30 @@
+import { } from 'dotenv/config';
+import { initializeApp } from "firebase/app";
+
+export const firebaseApp = () => {
+    // Firebase Config
+    const firebaseConfig = {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    }
+
+    // Initialize Firebase
+    const started = initializeApp(firebaseConfig);
+
+    // Check Authentification status of the user
+    started.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in.
+            const uid = user.uid;
+            console.log("User is signed in with UID: ", uid);
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in.");
+        }
+    });
+};
--- Assignment5/src/main.jsx
+++ Assignment5/src/main.jsx
@@ -1,24 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from './firebase_local/Configuration.jsx';
+import { firebaseApp } from './firebase_local/FB_App.jsx';
 import App from './App.jsx'
 import './assets/css/bootstrap.default.css';
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-
-// Check Authentification status of the user
-firebaseApp.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in.
-    const uid = user.uid;
-    console.log("User is signed in with uid: ", uid);
-  } else {
-    // No user is signed in.
-    console.log("No user is signed in.");
-  }
-});
+firebaseApp();
 
 // Initialize the root element
 createRoot(document.getElementById('root')).render(
