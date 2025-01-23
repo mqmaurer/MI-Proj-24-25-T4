a814f6bc2044fd91200a347f206f9e0db303254c
Elena Laubinger <elena_laubinger@stud.uni-bamberg.de>
2025-01-23 16:28:51 +0100

    Add global firebase settings

--- Assignment5/src/main.jsx
+++ Assignment5/src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/css/index.css'
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from './firebase_local/Configuration.jsx';
 import App from './App.jsx'
 
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Check Authentification status of the user
+firebaseApp.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in.
+    const uid = user.uid;
+    console.log("User is signed in with uid: ", uid);
+  } else {
+    // No user is signed in.
+    console.log("No user is signed in.");
+  }
+});
+
+// Initialize the root element
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+export default firebaseApp;
