2ecf2041b629a28110e16fe1684f4306175764af
Elena Laubinger <elena_laubinger@stud.uni-bamberg.de>
2025-01-23 17:06:12 +0100

    Minor import/rename changes

--- Assignment5/src/main.jsx
+++ Assignment5/src/main.jsx
@@ -1,23 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from './firebase_local/Configuration.jsx';
+import { firebaseApp } from './firebase_local/FB_App.jsx';
 import App from './App.jsx'
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-
-// Check Authentification status of the user
-firebaseApp.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in.
-    const uid = user.uid;
-    console.log("User is signed in with uid: ", uid);
-  } else {
-    // No user is signed in.
-    console.log("No user is signed in.");
-  }
-});
+firebaseApp();
 
 // Initialize the root element
 createRoot(document.getElementById('root')).render(
