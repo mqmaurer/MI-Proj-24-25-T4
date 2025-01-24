182acaee3080be8d15cf4ed350647ad9b9b1fe91
Anna-Lena Lumpp <anna-lena.lumpp@stud.uni-bamberg.de>
2025-01-24 13:35:26 +0100

    started with addBock

--- Assignment5/src/App.jsx
+++ Assignment5/src/App.jsx
@@ -1,17 +1,18 @@
-import './assets/css/App.css'
-import Header from './Modules/Header'
-import LocationTabs from './Modules/LocationTabs'
-import ViewSpace from './Modules/ViewSpace'
+import "./assets/css/App.css";
+import AddBook from "./Modules/AddBook";
+import Header from "./Modules/Header";
+import LocationTabs from "./Modules/LocationTabs";
+import ViewSpace from "./Modules/ViewSpace";
 
 function App() {
-
   return (
     <>
       <Header />
       <LocationTabs />
       <ViewSpace />
+      <AddBook />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
