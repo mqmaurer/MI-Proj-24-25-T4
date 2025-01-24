6517e316fa1a75e2ac9d14de777df965fc63cd3f
Elena Laubinger <Elena_Laubinger@stud.uni-bamberg.de>
2025-01-24 14:00:20 +0100

    Fix .env processing

--- Assignment5/src/firebase_local/FB_App.jsx
+++ Assignment5/src/firebase_local/FB_App.jsx
@@ -1,32 +1,19 @@
-import { } from 'dotenv/config';
 import { initializeApp } from "firebase/app";
 
-const firebaseApp = () => {
+export const firebaseApp = () => {
     // Firebase Config
     const firebaseConfig = {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        apiKey: import.meta.env.FIREBASE_API_KEY,
+        authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.FIREBASE_APP_ID,
+        measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
     }
 
     // Initialize Firebase
-    const started = initializeApp(firebaseConfig);
-
-    // Check Authentification status of the user
-    started.auth().onAuthStateChanged((user) => {
-        if (user) {
-            // User is signed in.
-            const uid = user.uid;
-            console.log("User is signed in with UID: ", uid);
-        } else {
-            // No user is signed in.
-            console.log("No user is signed in.");
-        }
-    });
+    const app = initializeApp(firebaseConfig);
+    
+    return app;
 };
-
-export default firebaseApp;
