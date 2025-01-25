f94391501d40074306ddab12111a97538dfeb162
Monique Maurer <monique.maurer@stud.uni-bamberg.de>
2025-01-25 21:46:48 +0100

    Database-Test

--- Assignment5/src/main.jsx
+++ Assignment5/src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { firebaseApp } from './firebase_local/FB_App.jsx';
 import App from './App.jsx'
 import './assets/css/bootstrap.default.css';
 
-// Initialize Firebase
-firebaseApp();
+
 
 // Initialize the root element
 createRoot(document.getElementById('root')).render(
