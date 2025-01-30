3d1508c04d04c22bd7ad6058130ff7ce4bd8eccb
Anna-Lena Lumpp <anna-lena.lumpp@stud.uni-bamberg.de>
2025-01-31 00:47:01 +0100

    had to comment out to make it work

--- Assignment5/src/App.jsx
+++ Assignment5/src/App.jsx
@@ -1,21 +1,16 @@
-import './assets/css/App.css'
-import Header from './Modules/Header'
-import LocationTabs from './Modules/LocationTabs'
-import ViewSpace from './Modules/ViewSpace'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import BookList from './Modules/BookList/BookList'
-import AddBook from './Modules/AddBook'
-import BookDetail from './Modules/BookDetail'
-import NotFound from './Modules/NotFound';
-import Database from './firebase_local/Database'; // Hook importieren
-
-
+import "./assets/css/App.css";
+import Header from "./Modules/Header";
+import LocationTabs from "./Modules/LocationTabs";
+import ViewSpace from "./Modules/ViewSpace";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import BookList from './Modules/BookList/BookList'
+import AddBook from "./Modules/AddBook/AddBook";
+import BookDetail from "./Modules/BookDetail";
+import NotFound from "./Modules/NotFound";
+import Database from "./firebase_local/Database"; // Hook importieren
 
 function App() {
-
-
   const books = Database();
-
 
   return (
     <Router>
@@ -25,7 +20,7 @@ function App() {
       <ViewSpace />
       <Routes>
         <Route path="/" element={<ViewSpace />} />
-        <Route path="/books" element={<BookList />} />
+        {/* <Route path="/books" element={<BookList />} /> */}
         <Route path="/addBooks" element={<AddBook />} />
         <Route path="/details" element={<BookDetail />} />
         <Route path="*" element={<NotFound />} />
