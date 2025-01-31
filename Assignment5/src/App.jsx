5809117ea6168544a7eaf083c7b50c382ed254a9
Elena Laubinger <elena_laubinger@stud.uni-bamberg.de>
2025-01-31 13:57:47 +0100

    Resolve Merge-Conflicts

--- Assignment5/src/App.jsx
+++ Assignment5/src/App.jsx
@@ -1,15 +1,18 @@
-import "./assets/css/App.css";
-import Header from "./Modules/Header";
-import LocationTabs from "./Modules/LocationTabs";
-import ViewSpace from "./Modules/ViewSpace";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import BookList from "./Modules/BookList/BookList";
-import AddBook from "./Modules/AddBook/AddBook";
-import BookDetail from "./Modules/BookDetail";
-import NotFound from "./Modules/NotFound";
-import Database from "./firebase_local/Database"; // Hook importieren
+import './assets/css/App.css'
+import Header from './Modules/Header'
+import LocationTabs from './Modules/LocationTabs'
+import ViewSpace from './Modules/ViewSpace'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import BookList from './Modules/BookList/BookList'
+import AddBook from './Modules/AddBook'
+import BookDetail from './Modules/BookDetail'
+import NotFound from './Modules/NotFound';
+import Database from './firebase_local/Database';
+
+
 
 function App() {
+
   const books = Database();
 
   return (
@@ -20,7 +23,7 @@ function App() {
       <ViewSpace />
       <Routes>
         <Route path="/" element={<ViewSpace />} />
-        {<Route path="/books" element={<BookList />} />}
+        <Route path="/books" element={<BookList books={books.data} />} />
         <Route path="/addBooks" element={<AddBook />} />
         <Route path="/details" element={<BookDetail />} />
         <Route path="*" element={<NotFound />} />
