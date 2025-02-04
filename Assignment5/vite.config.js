6b86b3fe7be3fde9e9e29bb379347ac5153eb7b5
Elena Laubinger <elena_laubinger@stud.uni-bamberg.de>
2025-02-04 22:04:26 +0100

    Edit Hosting workflow

--- Assignment5/vite.config.js
+++ Assignment5/vite.config.js
@@ -6,15 +6,10 @@ export default defineConfig(({ mode }) => {
     // Load environment variables from .env files
     const env = loadEnv(mode, process.cwd(), '')
 
-    // Set up environment variables with prefixes, so that vite can process them properly
-    const envWithProcessPrefix = Object.keys(env).reduce((prev, next) => {
-        prev[`process.env.${next}`] = JSON.stringify(env[next])
-        return prev
-    }, {})
-
     return {
         plugins: [react()],
-        define: envWithProcessPrefix
+        define: {
+            'process.env': env
+        }
     }
-}
-)
+})
